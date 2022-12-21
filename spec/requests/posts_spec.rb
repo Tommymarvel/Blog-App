cb4e9bdee784d285507264750abbc2d759543010
Tommymarvel <ibironketomiwa4@gmail.com>
2022-12-21 12:44:43 +0100

    update code

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Posts routing", type: :routing do
-  it "routes to the posts index" do
-    expect(:get => "/users/1/posts").to route_to("posts#index", :user_id => "1")
+RSpec.describe 'Posts routing', type: :routing do
+  it 'routes to the posts index' do
+    expect(get: '/users/1/posts').to route_to('posts#index', user_id: '1')
   end
 
-  it "routes to the post show" do
-    expect(:get => "/users/1/posts/1").to route_to("posts#show", :user_id => "1", :id => "1")
+  it 'routes to the post show' do
+    expect(get: '/users/1/posts/1').to route_to('posts#show', user_id: '1', id: '1')
   end
 
-  it "has named routes for the posts resource" do
-    expect(:get => user_posts_path(1)).to be_routable
-    expect(:get => user_post_path(1, 1)).to be_routable
+  it 'has named routes for the posts resource' do
+    expect(get: user_posts_path(1)).to be_routable
+    expect(get: user_post_path(1, 1)).to be_routable
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Users routing", type: :routing do
-  it "routes to the users index" do
-    expect(:get => "/users").to route_to("users#index")
+RSpec.describe 'Users routing', type: :routing do
+  it 'routes to the users index' do
+    expect(get: '/users').to route_to('users#index')
   end
 
-  it "routes to the user show" do
-    expect(:get => "/users/1").to route_to("users#show", :id => "1")
+  it 'routes to the user show' do
+    expect(get: '/users/1').to route_to('users#show', id: '1')
   end
 
-  it "has named routes for the users resource" do
-    expect(:get => users_path).to be_routable
-    expect(:get => user_path(1)).to be_routable
+  it 'has named routes for the users resource' do
+    expect(get: users_path).to be_routable
+    expect(get: user_path(1)).to be_routable
   end
 end
