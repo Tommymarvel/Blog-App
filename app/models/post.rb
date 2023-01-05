b97ea4a6480641428fc038479449a3c892245161
Tommymarvel <ibironketomiwa4@gmail.com>
2023-01-05 14:36:48 +0100

    Add tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :post_id
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
@@ -9,7 +10,7 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   def most_recent_comments
-    Comment.order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).includes(:post).limit(5)
   end
 
   private
